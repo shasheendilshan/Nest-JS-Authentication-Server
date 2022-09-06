18f76dd38c4cc76fb1c3069f5a3d6a7c03170730
unknown <shasheen@i-threesixty.com>
2022-09-06 11:40:41 +0530

    Install typeORM and postgres and configure database for the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,27 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      async useFactory(config: ConfigService) {
+        return {
+          type: 'postgres',
+          host: config.get('DB_HOST'),
+          username: config.get('DB_USER'),
+          password: config.get('DB_PASSWORD'),
+          port: +config.get('DB_PORT'),
+          database: config.get('DB_NAME'),
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: true,
+        };
+      },
+    }),
+  ],
 })
 export class AppModule {}
