bb01a62955d4b90cbd995271a543d4c5e9dc2748
unknown <shasheen@i-threesixty.com>
2022-09-06 14:34:29 +0530

    Install class validator and add user validation

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,9 +1,19 @@
+import { IsString, IsEmail, MinLength, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   firstName: string;
 
+  @IsString()
+  @IsNotEmpty()
   lastName: string;
 
+  @IsEmail()
   email: string;
 
+  @MinLength(8)
+  @IsNotEmpty()
+  @IsString()
   password: string;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  ParseUUIDPipe,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -26,17 +27,20 @@ export class UserController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
     return this.userService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
     return this.userService.update(id, updateUserDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.userService.remove(id);
   }
 }
